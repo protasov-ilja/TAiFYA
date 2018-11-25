7050fca4393c2f9240022d6af1d1c7f9149ad19d
Ilya Protasov <ilya.protasov@omega-r.com>
2018-11-25 19:29:11 +0300

    added determination class

---
+++ Determination/Determination/Determinator.cpp
@@ -0,0 +1,209 @@
+#include "stdafx.h"
+#include "Determinator.h"
+
+CDeterminator::CDeterminator(const std::string& inputfileName)
+{
+	ReadStateMachineInfoFromFile(inputfileName);
+	DeterminateStateMachine();
+}
+
+CDeterminator::~CDeterminator()
+{
+}
+
+void CDeterminator::ReadStateMachineInfoFromFile(const std::string& inputfileName)
+{
+	std::ifstream inputFile(inputfileName);
+	if (!inputFile.is_open())
+	{
+		std::cout << "Can't open the file for reading!" << inputfileName << std::endl;
+
+		return;
+	}
+
+	inputFile >> _numberOfInputSignals;
+	inputFile >> _numberOfStates;
+	inputFile >> _numberOfFinalStates;
+	for (int i = 0; i < _numberOfFinalStates; ++i)
+	{
+		int tempNumb;
+		inputFile >> tempNumb;
+		_finalStates.insert(tempNumb);
+	}
+
+	ReadStateMachineFromFile(inputFile);
+}
+
+void CDeterminator::ReadStateMachineFromFile(std::ifstream& inputFile)
+{
+	_initialStateMachine = GetEmptyStateMachine(_numberOfStates, _numberOfInputSignals);
+	std::string str;
+	std::getline(inputFile, str);
+	int index = 0;
+	while (std::getline(inputFile, str))
+	{
+		if (index < _numberOfStates)
+		{
+			std::stringstream stream(str);
+			int state;
+			int signal;
+			while (stream >> state >> signal)
+			{
+				_initialStateMachine[index][signal].states.insert(state);
+			}
+		}
+		
+		index++;
+	}
+}
+
+std::vector<std::vector<StateMachineState>> CDeterminator::GetEmptyStateMachine(int numberOfStates, int numberOfInputSignals)
+{
+	std::vector<std::vector<StateMachineState>> stateMachine(numberOfStates, std::vector<StateMachineState>(numberOfInputSignals));
+	for (int i = 0; i < stateMachine.size(); ++i)
+	{
+		for (int j = 0; j < stateMachine[i].size(); ++j)
+		{
+			StateMachineState st;
+			stateMachine[i][j] = st;
+		}
+	}
+
+	return stateMachine;
+}
+
+void CDeterminator::DeterminateStateMachine()
+{
+	std::queue<std::set<int>> statesQueue;
+	_statesList.insert(std::pair<std::set<int>, int>({ 0 }, 0));
+	std::set<int> set = { 0 };
+	AddNewState(set, statesQueue);
+	while (statesQueue.size() != 0) 
+	{
+		std::set<int> state = statesQueue.front();
+		statesQueue.pop();
+		AddNewState(state, statesQueue);
+	}
+}
+
+void CDeterminator::AddNewState(const std::set<int>& initStates, std::queue<std::set<int>>& queue)
+{
+	std::vector<StateMachineState> states(_numberOfInputSignals);
+	for (int i = 0; i < _initialStateMachine[0].size(); ++i)
+	{
+		InsertStatesInState(initStates, states[i], i);
+		if (states[i].states.size() != 0)
+		{
+			auto search = _statesList.find(states[i].states);
+			if (search == _statesList.end())
+			{
+				states[i].stateIndex = _statesList.size();
+				_statesList.insert(std::pair<std::set<int>, int>(states[i].states, states[i].stateIndex));
+				queue.push(states[i].states);
+				AddStateInFinalStates(states[i]);
+			}
+			else 
+			{
+				states[i].stateIndex = search -> second;
+			}
+		}
+	}
+
+	_determinatedStateMachine.push_back(states);
+}
+
+void CDeterminator::InsertStatesInState(const std::set<int>& initStates, StateMachineState& state, int initStateIndex)
+{
+	bool isFirstIteration = false;
+	for (auto initState : initStates)
+	{
+		if (isFirstIteration)
+		{
+			StateMachineState st;
+			state = st;
+		}
+
+		for (auto st : _initialStateMachine[initState][initStateIndex].states)
+		{
+			state.states.insert(st);
+		}
+	}
+}
+
+void CDeterminator::AddStateInFinalStates(StateMachineState state)
+{
+	for (auto finalState : _finalStates)
+	{
+		if (state.states.count(finalState))
+		{
+			_determinatedFinalStates.insert(state.stateIndex);
+			return;
+		}
+	}
+}
+
+void CDeterminator::ShowDeterminatedStateMachine(const std::string& outputFileName)
+{
+	std::ofstream outputFile(outputFileName);
+	if (!outputFile.is_open())
+	{
+		std::cout << "Can't open the file for writing!" << outputFileName << std::endl;
+
+		return;
+	}
+
+	outputFile << _numberOfInputSignals << std::endl;
+	outputFile << _numberOfStates << std::endl;
+	outputFile << _determinatedFinalStates.size() << std::endl;
+	for (auto finalState : _determinatedFinalStates)
+	{
+		outputFile << finalState << " ";
+	}
+
+	outputFile << std::endl;
+	for (int i = 0; i < _determinatedStateMachine.size(); ++i)
+	{
+		for (int j = 0; j < _determinatedStateMachine[i].size(); ++j)
+		{
+			outputFile << _determinatedStateMachine[i][j].stateIndex << " ";
+		}
+
+		outputFile << std::endl;
+	}
+}
+
+void CDeterminator::CreateDotFile()
+{
+	using Edge = std::pair<int, int>;
+	using Graph = boost::adjacency_list<boost::vecS,
+		boost::vecS, boost::directedS,
+		boost::property<boost::vertex_color_t,
+		boost::default_color_type>,
+		boost::property<boost::edge_weight_t, std::string>>;
+
+	std::vector<Edge> edges;
+	std::vector<std::string> weights(edges.size());
+	const int VERTEX_COUNT = _determinatedStateMachine.size();
+	for (size_t i = 0; i < _determinatedStateMachine.size(); ++i)
+	{
+		for (size_t j = 0; j < _determinatedStateMachine[i].size(); ++j)
+		{
+			if (_determinatedStateMachine[i][j].stateIndex != -1)
+			{
+				Edge edge(i, _determinatedStateMachine[i][j].stateIndex);
+				edges.push_back(edge);
+				weights.push_back(std::to_string(j));
+			}
+		}
+	}
+
+	Graph graph(edges.begin(), edges.end(), weights.begin(),
+		VERTEX_COUNT);
+
+	boost::dynamic_properties dp;
+	dp.property("weight", boost::get(boost::edge_weight, graph));
+	dp.property("label", boost::get(boost::edge_weight, graph));
+	dp.property("node_id", boost::get(boost::vertex_index, graph));
+	std::ofstream ofs("test.dot");
+	boost::write_graphviz_dp(ofs, graph, dp);
+}
---
+++ Determination/Determination/Determinator.h
@@ -0,0 +1,35 @@
+#pragma once
+
+struct StateMachineState
+{
+	int stateIndex = -1;
+	std::set<int> states;
+};
+
+class CDeterminator
+{
+public:
+	CDeterminator(const std::string& inputfileName);
+	~CDeterminator();
+	void ShowDeterminatedStateMachine(const std::string& outputFileName);
+	void CreateDotFile();
+
+private:
+	void ReadStateMachineInfoFromFile(const std::string& fileName);
+	void ReadStateMachineFromFile(std::ifstream& inputFile);
+	void DeterminateStateMachine();
+	void AddNewState(const  std::set<int>& initStates, std::queue<std::set<int>>& queue);
+	void AddStateInFinalStates(StateMachineState state);
+	std::vector<std::vector<StateMachineState>> GetEmptyStateMachine(int numberOfStates, int numberOfInputSignals);
+	void InsertStatesInState(const std::set<int>& initStates, StateMachineState& state, int initStateIndex);
+
+	int _numberOfInputSignals;
+	int _numberOfStates;
+	int _numberOfFinalStates;
+	std::set<int> _finalStates;
+	std::set<int> _determinatedFinalStates;
+	std::vector<std::vector<StateMachineState>> _initialStateMachine;
+	std::map<std::set<int>, int> _statesList;
+	std::vector<std::vector<StateMachineState>> _determinatedStateMachine;
+};
+
