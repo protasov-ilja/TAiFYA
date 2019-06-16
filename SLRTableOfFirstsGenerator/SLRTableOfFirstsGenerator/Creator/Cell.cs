d12b5836523c378e094c866d0a640515c610effa
Ilya Protasov <ilya.protasov@omega-r.com>
2019-06-16 16:31:58 +0300

    added summary for cell class fields

--- SLRTableOfFirstsGenerator/SLRTableOfFirstsGenerator/Creator/Cell.cs
+++ SLRTableOfFirstsGenerator/SLRTableOfFirstsGenerator/Creator/Cell.cs
@@ -6,12 +6,20 @@ namespace SLRTableOfFirstsGenerator.Creator
 	{
 		private readonly List<Token> _values = new List<Token>();
 
+		/// <summary>
+		/// get value of the cell by index
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
 		public Token this[int index]
 		{
 			get => _values[index];
 			set => _values[index] = value;
 		}
 
+		/// <summary>
+		/// value of the cell
+		/// </summary>
 		public string Value => _values.Count != 0 ? _values[0].Value : ""; 
 
 		public Cell(Token token)
